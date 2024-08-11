a332f667ad3adf0aaa257899401c89017c06d3bb
ananito <anisfhomsi@gmail.com>
2024-08-11 10:36:23 +0300

    part1 c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0)
+  const addToCounter = () => {
+    return setCounter(counter + 1)
+  }
+  const restToZero = () => setCounter(0);
 
   return (
     <div>
-      <p>Hello, World!</p>
-    </div>
+      <h1>{counter}</h1>
 
+      <br />
+      <button onClick={addToCounter}>Add to counter</button>
+      <br />
+      <button onClick={restToZero}>Reset Counter</button>
+
+    </div>
   )
 }
 
