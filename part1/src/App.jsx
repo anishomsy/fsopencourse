374df5d57ef80e4773ae9df342bc176e1c073676
ananito <anisfhomsi@gmail.com>
2024-08-12 15:29:41 +0300

    complete part1 c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,43 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => {
+  return (<div>{counter}</div>);
+}
+
+export function Button({ onClick, text }) {
+  return (<button onClick={onClick} > {text}</button >);
+}
+
+
+
 const App = () => {
 
+
   const [counter, setCounter] = useState(0)
+  console.log("rendering with counter values", counter);
   const addToCounter = () => {
+    console.log("increasingm value before: ", counter);
     return setCounter(counter + 1)
   }
-  const restToZero = () => setCounter(0);
+  const restToZero = () => { console.log("to zero value before", counter); setCounter(0); }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  }
 
   return (
     <div>
-      <h1>{counter}</h1>
+
+      <Display counter={counter} />
 
       <br />
-      <button onClick={addToCounter}>Add to counter</button>
+      <Button onClick={addToCounter} text="Add to counter" />
       <br />
-      <button onClick={restToZero}>Reset Counter</button>
+
+      <Button onClick={restToZero} text="Reset to zero" />
+      <br />
+      <Button onClick={decreaseByOne} text="Decrease by one" />
 
     </div>
   )
